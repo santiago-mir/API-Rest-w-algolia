a8d1b539cb022b350996de6f40df97d99999db2b
santiago-mir <95775156+santiago-mir@users.noreply.github.com>
2023-12-11 20:41:25 -0300

    Delete lib directory

--- lib/algolia.ts
+++
@@ -1,9 +0,0 @@
-import algoliasearch from "algoliasearch";
-
-// Connect and authenticate with your Algolia app
-const client = algoliasearch("LODOCO1MRH", "817015e9902a8df4e57968630f7acc42");
-
-// Create a new index and add a record
-const index = client.initIndex("products");
-
-export { index };
